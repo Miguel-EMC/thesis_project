4fa6ed5a796a327f1d1c08d41c853ed0c26d4d1f
Miguel-EMC <eduardomuzo123456@gmail.com>
2022-12-07 16:09:08 -0500

    Model Producto

---
+++ app/Models/Product.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'price_min',
+        'price_max',
+        'detail',
+        'stock',
+        'state_appliance',
+        'state',
+        'delivery_method',
+        'brand',
+    ];
+}
---
+++ database/migrations/2022_12_07_160307_create_products_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            // ID para la tabla Appliances de la BD
+            $table->bigIncrements('id');
+            // columna para la tabla Appliances de la BD
+            $table->string('title', 150);
+            // precio minimo del electrodomestico
+            $table->float('price_min', 8, 2);
+            // precio maximo del electrodomestico
+            $table->float('price_max', 8, 2);
+            // detalle sobre el electrodomestico
+            $table->string('detail', 150);
+            // stock del electrodomestico
+            $table->integer('stock');
+            // estado del electrodomestico
+            $table->string('state_appliance', 50);
+
+            //columna para conocer el estado del producto
+            $table->boolean('state')->default(true);
+            
+            // columnas que se podran aceptar como registros null en la tabla Appliances de la BD
+            // metodo de envio del electrodomestico
+            $table->string('delivery_method', 150)->nullable();
+            // marca del electrodomestico
+            $table->string('brand', 50)->nullable();
+
+            //columna para conocer la fecha de creacion y actualizacion de los registros
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
