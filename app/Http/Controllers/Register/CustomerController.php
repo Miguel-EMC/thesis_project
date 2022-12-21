8e0ba73d296fb89ae819bd4cf0a05eca8715eeac
Miguel-EMC <eduardomuzo123456@gmail.com>
2022-12-21 17:54:51 -0500

    Correcion token

--- app/Http/Controllers/Register/CustomerController.php
+++ app/Http/Controllers/Register/CustomerController.php
@@ -17,17 +17,13 @@ class CustomerController extends Controller
     {
         // Validación de los datos de entrada
         $request->validate([
+            'username' => ['required', 'string', 'min:5', 'max:20', 'unique:users'],
             'first_name' => ['required', 'string', 'min:3', 'max:35'],
             'last_name' => ['required', 'string', 'min:3', 'max:35'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'home_phone' => ['nullable', 'numeric', 'digits:7'],
             'personal_phone' => ['required', 'numeric', 'digits:10'],
             'address' => ['required', 'string', 'min:5', 'max:50'],
-
-
-            'cedula' => ['nullable', 'numeric', 'digits:10', 'unique:users'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'username' => ['required', 'string', 'min:5', 'max:20', 'unique:users'],
-
-            'home_phone' => ['nullable', 'numeric', 'digits:7']
         ]);
 
         // Validación de los datos de entrada
