caf01008d938954d9a2b0b5fc40f20f07161a242
Miguel-EMC <eduardomuzo123456@gmail.com>
2022-12-22 01:37:42 -0500

    Rutas para el chat interno

--- routes/api.php
+++ routes/api.php
@@ -63,7 +63,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
         }
 
     );
-    // Se hace uso de grupo de rutas para el chat entre usuarios
+    // Se hace uso de grupo de rutas para el chat
     Route::controller(MessageController::class)->group(
         function () {
             Route::get('user/contacts', 'getContacts')->name('chat.getContacts');
