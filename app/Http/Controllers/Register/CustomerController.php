2292f0fedae3deaaee8f3368c8425d8ecbd949b1
Miguel-EMC <eduardomuzo123456@gmail.com>
2023-01-19 01:00:14 -0500

    Pruebas

--- app/Http/Controllers/Register/CustomerController.php
+++ app/Http/Controllers/Register/CustomerController.php
@@ -17,13 +17,13 @@ class CustomerController extends Controller
     {
         // Validación de los datos de entrada
         $request->validate([
-            'username' => ['required', 'string', 'min:5', 'max:20'],
-            // 'first_name' => ['required', 'string', 'min:3', 'max:35'],
-            // 'last_name' => ['required', 'string', 'min:3', 'max:35'],
-            'email' => ['required', 'string', 'email', 'max:255'],
-            // 'home_phone' => ['nullable', 'numeric', 'digits:7'],
-            // 'personal_phone' => ['required', 'numeric', 'digits:10'],
-            // 'address' => ['required', 'string', 'min:5', 'max:50'],
+            'username' => ['required', 'string', 'min:5', 'max:20', 'unique:users'],
+            'first_name' => ['required', 'string', 'min:3', 'max:35'],
+            'last_name' => ['required', 'string', 'min:3', 'max:35'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'home_phone' => ['nullable', 'numeric', 'digits:7'],
+            'personal_phone' => ['required', 'numeric', 'digits:10'],
+            'address' => ['required', 'string', 'min:5', 'max:50'],
         ]);
 
         // Validación de los datos de entrada
