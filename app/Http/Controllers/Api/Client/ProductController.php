38757afcc48a09fb1663d76fba4a16316f2ef81f
Miguel-EMC <eduardomuzo123456@gmail.com>
2023-01-23 01:37:02 -0500

    Cambios

--- app/Http/Controllers/Api/Client/ProductController.php
+++ app/Http/Controllers/Api/Client/ProductController.php
@@ -26,16 +26,7 @@ class ProductController extends Controller
     //Funcion para mostrar todos los productos de la base de datos
     public function index()
     {
-        $products = Cache::remember('products_query', 60, function () {
-            return Product::active()->get();
-        });
-
-        return $this->sendResponse(
-        message: "Products returned successfully",
-        result: [
-                'products' => new ProductCollection($products),
-            ]
-        );
+        return new ProductCollection(Product::paginate(25));
     }
 
     //Funcion para mostrar un producto en especifico
--- app/Http/Resources/ProductCollection.php
+++ app/Http/Resources/ProductCollection.php
@@ -17,7 +17,7 @@ class ProductCollection extends ResourceCollection
         return [
             'data' => $this->collection,
             'links' => [
-                'self' => 'link-value',
+                'self' => 'link',
             ],
         ];
     }
